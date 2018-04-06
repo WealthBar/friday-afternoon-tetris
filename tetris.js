f0bf5d59c35f375b7aa086514b4cbcdf9ac8d5ac
Jean-Francois Arseneau <jf.arseneau@wealthbar.com>
2018-04-06 14:40:49 -0700

    Improve colouring of Tetromino

--- tetris.js
+++ tetris.js
@@ -161,17 +161,24 @@
             w,
           );
 
-          ctx.fillRect(
-            x,
-            y,
-            pieceWidth,
-            pieceHeight,
-          );
+          // ctx.fillRect(
+          //   x,
+          //   y,
+          //   pieceWidth,
+          //   pieceHeight,
+          // );
         }
       }
     }
   }
 
+  function pickTetrominoColor(x, y) {
+    let gradient = ctx.createRadialGradient(x,y,10,x+1,y+1,40);
+    gradient.addColorStop(0,"red");
+    // TODO: Cycle through colors
+    gradient.addColorStop(1,"white");
+    return gradient;
+  }
   function drawTetromino() {
     currentTetromino = tetrominos[droppingTetromino].piece;
     for (let i = 0; i < currentTetromino.length; i++) {
@@ -183,6 +190,8 @@
           droppingWidth + j,
         );
 
+        let gradient = pickTetrominoColor(x, y);
+        ctx.fillStyle = gradient;
         ctx.fillRect(
           x,
           y,
