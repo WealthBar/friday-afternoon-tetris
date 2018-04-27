0f0ac4998618713fc213a385858b97c9b90b5d72
Mel Delosada <mel.delosada@wealthbar.com>
2018-04-27 14:09:43 -0700

    add color list

--- src/globals.js
+++ src/globals.js
@@ -1,10 +1,10 @@
 const wellWidth = 10;
 const wellHeight = 22;
-const pieceUp=0;
-const pieceRight=1;
-const pieceDown=2;
-const pieceLeft=3;
-const pieceMax=4;
+const pieceUp = 0;
+const pieceRight = 1;
+const pieceDown = 2;
+const pieceLeft = 3;
+const pieceMax = 4;
 
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
@@ -14,3 +14,6 @@ let droppingDelta = 0;
 let droppingRate = 500;
 let droppingTetromino = 1;
 let pieceOrientation = pieceDown;
+
+let trackedTetrominoPosition;
+let currentTetrominoGradient;
