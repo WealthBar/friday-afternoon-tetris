eae7ef0f9e0e823284f970d0d5915cf4851fede1
Cliff Hammerschmidt <cliff.hammerschmidt@wealthbar.com>
2018-04-27 13:17:01 -0700

    add rotateCw and Ccw

--- src/tetrominos.js
+++ src/tetrominos.js
@@ -80,3 +80,34 @@ const tetrominos =
       ],
     ],
   ];
+
+
+function rotateCw(matrix) {
+  const w = matrix.length;
+  const h = matrix[0].length;
+
+  const result = [];
+  for (let hi = 0; hi < h; ++hi) {
+    const row = [];
+    result.push(row);
+    for (let wi = w - 1; wi >= 0; --wi) {
+      row.push(matrix[wi][hi]);
+    }
+  }
+  return result;
+}
+
+function rotateCcw(matrix) {
+  const w = matrix.length;
+  const h = matrix[0].length;
+
+  const result = [];
+  for (let hi = h-1; hi >= 0; --hi) {
+    const row = [];
+    result.push(row);
+    for (let wi = 0; wi < w; ++wi) {
+      row.push(matrix[wi][hi]);
+    }
+  }
+  return result;
+}
